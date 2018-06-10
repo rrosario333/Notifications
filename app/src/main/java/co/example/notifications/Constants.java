f42f75f4231fb887d281120100c2a78d3de72de1
Robert Rosario <Robert Rosario@RobertRosario.Home>
2018-06-11 00:31:12 +0100

    test

---
+++ app/src/main/java/co/example/notifications/Constants.java
@@ -0,0 +1,12 @@
+package co.example.notifications;
+
+/**
+ * Created by L on 09/05/2017.
+ * Copyright (c) 2017 Centroida. All rights reserved.
+ */
+
+public class Constants {
+
+    public static final String FIREBASE_TOKEN = "firebase token";
+
+}
